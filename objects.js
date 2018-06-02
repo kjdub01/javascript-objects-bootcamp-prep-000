4429261f8e6bcdfd11caace02311bf776625d578
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-06-02 03:13:32 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = { Drake: 'Nice for What', BTS: 'Fake Love'};
+
+function updatePlaylist(playlist, name, title) {
+  return Object.assign( {}, playlist, { [Bazzi]: 'Mine'})
+}
+
+function removeFromPlaylist(playlist, name) {
+  return delete playlist.BTS
+}
+
