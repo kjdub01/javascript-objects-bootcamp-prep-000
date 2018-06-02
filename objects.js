cba1dd1d71cae17ac22dd9b6cbe1a3a418f4d2c7
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-06-02 03:22:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { 'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'};
 
 function updatePlaylist(playlist, name, title) {
-  return Object.assign( {}, playlist, { ["Phil Ochs"]: "Here's to the State of Mississippi"})
+  return playlist['Phil Ochs'] = "Here's to the State of Mississippi";
 }
 
 function removeFromPlaylist(playlist, name) {
