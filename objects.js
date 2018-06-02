e7041f0d48f2eea0ac04c29adfa62598631e16ba
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-06-02 03:15:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = { Slowdive: 'Alison', BTS: 'Fake Love'};
+var playlist = { 'Slowdive': 'Alison', 'My Bloody Valentine': 'Sometimes'};
 
 function updatePlaylist(playlist, name, title) {
   return Object.assign( {}, playlist, { [Bazzi]: 'Mine'})
